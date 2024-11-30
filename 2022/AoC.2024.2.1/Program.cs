15c57fc364f9c57f3f8ea1505de81953d648eac9
Brett Lyons <ultimojones@gmail.com>
2024-11-30 16:38:47 +1100

    🤠

---
+++ 2022/AoC.2024.2.1/Program.cs
@@ -0,0 +1,29 @@
+﻿var file = Debugger.IsAttached ? "example.txt" : "input.txt";
+
+long score = 0;
+
+foreach (var line in File.ReadLines(file))
+{
+    score +=
+        line[2] switch {
+            'X' => 1,
+            'Y' => 2,
+            'Z' => 3,
+            _ => 0
+        } +
+        (line[0], line[2]) switch
+        {
+            ('A', 'Z') => 0,
+            ('B', 'X') => 0,
+            ('C', 'Y') => 0,
+            ('A', 'X') => 3,
+            ('B', 'Y') => 3,
+            ('C', 'Z') => 3,
+            ('A', 'Y') => 6,
+            ('B', 'Z') => 6,
+            ('C', 'X') => 6,
+            _ => 0
+        };
+}
+
+Console.WriteLine(new { score });
---
+++ 2022/AoC.2024.2.2/Program.cs
@@ -0,0 +1,36 @@
+﻿var file = Debugger.IsAttached ? "example.txt" : "input.txt";
+
+long score = 0;
+
+foreach (var line in File.ReadLines(file))
+{
+    var play = (line[0], line[2]) switch
+    {
+        ('A', 'X') => 'Z',
+        ('A', 'Y') => 'X',
+        ('A', 'Z') => 'Y',
+        ('B', 'X') => 'X',
+        ('B', 'Y') => 'Y',
+        ('B', 'Z') => 'Z',
+        ('C', 'X') => 'Y',
+        ('C', 'Y') => 'Z',
+        ('C', 'Z') => 'X',
+        _ => '\0'
+    };
+    score +=
+        play switch {
+            'X' => 1,
+            'Y' => 2,
+            'Z' => 3,
+            _ => 0
+        } +
+        line[2] switch
+        {
+            'X' => 0,
+            'Y' => 3,
+            'Z' => 6,
+            _ => 0
+        };
+}
+
+Console.WriteLine(new { score });
