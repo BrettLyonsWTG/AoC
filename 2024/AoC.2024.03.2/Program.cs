40dd3e4e709f3a0e93aa2a50a9e17eb45c8aef28
Brett Lyons <Brett.Lyons@wisetechglobal.com>
2024-12-03 16:48:08 +1100

    🤠

---
+++ 2024/AoC.2024.03.2/Program.cs
@@ -0,0 +1,24 @@
+﻿var file = Debugger.IsAttached ? "example.txt" : "input.txt";
+
+var input = File.ReadAllText(file);
+bool enabled = true;
+int result = 0;
+
+foreach (Match m in Regex.Matches(input, @"mul\((\d+),(\d+)\)|do\(\)|don\'t\(\)"))
+{
+	switch (m.ValueSpan)
+	{
+		case "do()":
+			enabled = true;
+			break;
+		case "don't()":
+			enabled = false;
+			break;
+		default:
+			if (enabled)
+				result += int.Parse(m.Groups[1].ValueSpan) * int.Parse(m.Groups[2].ValueSpan);
+			break;
+	}
+}
+
+Console.WriteLine(new { result });
