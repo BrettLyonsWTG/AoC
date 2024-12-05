47e06ddedb6696ab28f2ae3f44cc8d4b1ec2aa7d
Brett Lyons <ultimojones@gmail.com>
2024-12-05 17:41:49 +1100

    🤠

--- 2024/AoC.2024.04.1/Program.cs
+++ 2024/AoC.2024.04.1/Program.cs
@@ -4,24 +4,51 @@ var grid = File.ReadLines(file).SelectMany((line, y) => line.Select((c, x) => (x
 
 var maxx = grid.Max(g => g.Key.x);
 var maxy = grid.Max(g => g.Key.y);
+var gridx = grid.Where(g => g.Value is 'X').Select(g => g.Key).ToList();
 
-var result = 
-	grid.Where(g => g.Value is 'X' && g.Key.x < maxx - 3 
-		&& grid[(g.Key.x + 1, g.Key.y)] is 'M' && grid[(g.Key.x + 2, g.Key.y)] is 'A' && grid[(g.Key.x + 3, g.Key.y)] is 'S').Concat(
-	grid.Where(g => g.Value is 'X' && g.Key.x > 2 
-		&& grid[(g.Key.x - 1, g.Key.y)] is 'M' && grid[(g.Key.x - 2, g.Key.y)] is 'A' && grid[(g.Key.x - 3, g.Key.y)] is 'S')).Concat(
-	grid.Where(g => g.Value is 'X' && g.Key.y < maxy - 3 
-		&& grid[(g.Key.x, g.Key.y + 1)] is 'M' && grid[(g.Key.x, g.Key.y + 2)] is 'A' && grid[(g.Key.x, g.Key.y + 3)] is 'S')).Concat(
-	grid.Where(g => g.Value is 'X' && g.Key.y > 2 
-		&& grid[(g.Key.x, g.Key.y - 1)] is 'M' && grid[(g.Key.x, g.Key.y - 2)] is 'A' && grid[(g.Key.x, g.Key.y - 3)] is 'S')).Concat(
-	grid.Where(g => g.Value is 'X' && g.Key.x < maxx - 3 && g.Key.y < maxy - 3
-		&& grid[(g.Key.x + 1, g.Key.y + 1)] is 'M' && grid[(g.Key.x + 2, g.Key.y + 2)] is 'A' && grid[(g.Key.x + 3, g.Key.y + 3)] is 'S')).Concat(
-	grid.Where(g => g.Value is 'X' && g.Key.x > 2 && g.Key.y < maxy - 3
-		&& grid[(g.Key.x - 1, g.Key.y + 1)] is 'M' && grid[(g.Key.x - 2, g.Key.y + 2)] is 'A' && grid[(g.Key.x - 3, g.Key.y + 3)] is 'S')).Concat(
-	grid.Where(g => g.Value is 'X' && g.Key.x < maxx - 3 && g.Key.y > 2
-		&& grid[(g.Key.x + 1, g.Key.y - 1)] is 'M' && grid[(g.Key.x + 2, g.Key.y - 2)] is 'A' && grid[(g.Key.x + 3, g.Key.y - 3)] is 'S')).Concat(
-	grid.Where(g => g.Value is 'X' && g.Key.x > 2 && g.Key.y > 2
-		&& grid[(g.Key.x - 1, g.Key.y - 1)] is 'M' && grid[(g.Key.x - 2, g.Key.y - 2)] is 'A' && grid[(g.Key.x - 3, g.Key.y - 3)] is 'S')).Count();
+var r = gridx.Where(g => g.x < maxx - 2 && grid[(g.x + 1, g.y)] is 'M' && grid[(g.x + 2, g.y)] is 'A' && grid[(g.x + 3, g.y)] is 'S').ToList();
+var l = gridx.Where(g => g.x > 2 && grid[(g.x - 1, g.y)] is 'M' && grid[(g.x - 2, g.y)] is 'A' && grid[(g.x - 3, g.y)] is 'S').ToList();
+var d = gridx.Where(g => g.y < maxy - 2 && grid[(g.x, g.y + 1)] is 'M' && grid[(g.x, g.y + 2)] is 'A' && grid[(g.x, g.y + 3)] is 'S').ToList();
+var u = gridx.Where(g => g.y > 2 && grid[(g.x, g.y - 1)] is 'M' && grid[(g.x, g.y - 2)] is 'A' && grid[(g.x, g.y - 3)] is 'S').ToList();
+var dr = gridx.Where(g => g.x < maxx - 2 && g.y < maxy - 2 && grid[(g.x + 1, g.y + 1)] is 'M' && grid[(g.x + 2, g.y + 2)] is 'A' && grid[(g.x + 3, g.y + 3)] is 'S').ToList();
+var dl = gridx.Where(g => g.x > 2 && g.y < maxy - 2 && grid[(g.x - 1, g.y + 1)] is 'M' && grid[(g.x - 2, g.y + 2)] is 'A' && grid[(g.x - 3, g.y + 3)] is 'S').ToList();
+var ur = gridx.Where(g => g.x < maxx - 2 && g.y > 2 && grid[(g.x + 1, g.y - 1)] is 'M' && grid[(g.x + 2, g.y - 2)] is 'A' && grid[(g.x + 3, g.y - 3)] is 'S').ToList();
+var ul = gridx.Where(g => g.x > 2 && g.y > 2 && grid[(g.x - 1, g.y - 1)] is 'M' && grid[(g.x - 2, g.y - 2)] is 'A' && grid[(g.x - 3, g.y - 3)] is 'S').ToList();
+
+for (var y = 0; y <= maxy; y++)
+{
+    for (var x = 0; x <= maxx; x++)
+    {
+        if (r.Intersect([(x, y), (x - 1, y), (x - 2, y), (x - 3, y)]).Any())
+            Console.Write(grid[(x, y)]);
+        else
+        if (l.Intersect([(x, y), (x + 1, y), (x + 2, y), (x + 3, y)]).Any())
+            Console.Write(grid[(x, y)]);
+        else
+        if (d.Intersect([(x, y), (x, y - 1), (x, y - 2), (x, y - 3)]).Any())
+            Console.Write(grid[(x, y)]);
+        else
+        if (u.Intersect([(x, y), (x, y + 1), (x, y + 2), (x, y + 3)]).Any())
+            Console.Write(grid[(x, y)]);
+        else
+        if (dr.Intersect([(x, y), (x - 1, y - 1), (x - 2, y - 2), (x - 3, y - 3)]).Any())
+            Console.Write(grid[(x, y)]);
+        else
+        if (dl.Intersect([(x, y), (x + 1, y - 1), (x + 2, y - 2), (x + 3, y - 3)]).Any())
+            Console.Write(grid[(x, y)]);
+        else
+        if (ur.Intersect([(x, y), (x - 1, y + 1), (x - 2, y + 2), (x - 3, y + 3)]).Any())
+            Console.Write(grid[(x, y)]);
+        else
+        if (ul.Intersect([(x, y), (x + 1, y + 1), (x + 2, y + 2), (x + 3, y + 3)]).Any())
+            Console.Write(grid[(x, y)]);
+        else
+            Console.Write('.');
+    }
+    Console.WriteLine();
+}
+
+var result = r.Count + l.Count + d.Count + u.Count + dr.Count + dl.Count + ur.Count + ul.Count;
 
 Console.WriteLine(new { result });
 
--- 2024/AoC.2024.04.2/Program.cs
+++ 2024/AoC.2024.04.2/Program.cs
@@ -1,1 +1,55 @@
 ﻿var file = Debugger.IsAttached ? "example.txt" : "input.txt";
+
+var grid = File.ReadLines(file).SelectMany((line, y) => line.Select((c, x) => (x, y, c))).ToDictionary(g => (g.x, g.y), g => g.c);
+
+var maxx = grid.Max(g => g.Key.x);
+var maxy = grid.Max(g => g.Key.y);
+var gridx = grid.Where(g => g.Value is 'X').Select(g => g.Key).ToList();
+
+var r = gridx.Where(g => g.x < maxx - 2 && grid[(g.x + 1, g.y)] is 'M' && grid[(g.x + 2, g.y)] is 'A' && grid[(g.x + 3, g.y)] is 'S').ToList();
+var l = gridx.Where(g => g.x > 2 && grid[(g.x - 1, g.y)] is 'M' && grid[(g.x - 2, g.y)] is 'A' && grid[(g.x - 3, g.y)] is 'S').ToList();
+var d = gridx.Where(g => g.y < maxy - 2 && grid[(g.x, g.y + 1)] is 'M' && grid[(g.x, g.y + 2)] is 'A' && grid[(g.x, g.y + 3)] is 'S').ToList();
+var u = gridx.Where(g => g.y > 2 && grid[(g.x, g.y - 1)] is 'M' && grid[(g.x, g.y - 2)] is 'A' && grid[(g.x, g.y - 3)] is 'S').ToList();
+var dr = gridx.Where(g => g.x < maxx - 2 && g.y < maxy - 2 && grid[(g.x + 1, g.y + 1)] is 'M' && grid[(g.x + 2, g.y + 2)] is 'A' && grid[(g.x + 3, g.y + 3)] is 'S').ToList();
+var dl = gridx.Where(g => g.x > 2 && g.y < maxy - 2 && grid[(g.x - 1, g.y + 1)] is 'M' && grid[(g.x - 2, g.y + 2)] is 'A' && grid[(g.x - 3, g.y + 3)] is 'S').ToList();
+var ur = gridx.Where(g => g.x < maxx - 2 && g.y > 2 && grid[(g.x + 1, g.y - 1)] is 'M' && grid[(g.x + 2, g.y - 2)] is 'A' && grid[(g.x + 3, g.y - 3)] is 'S').ToList();
+var ul = gridx.Where(g => g.x > 2 && g.y > 2 && grid[(g.x - 1, g.y - 1)] is 'M' && grid[(g.x - 2, g.y - 2)] is 'A' && grid[(g.x - 3, g.y - 3)] is 'S').ToList();
+
+for (var y = 0; y <= maxy; y++)
+{
+    for (var x = 0; x <= maxx; x++)
+    {
+        if (r.Intersect([(x, y), (x - 1, y), (x - 2, y), (x - 3, y)]).Any())
+            Console.Write(grid[(x, y)]);
+        else
+        if (l.Intersect([(x, y), (x + 1, y), (x + 2, y), (x + 3, y)]).Any())
+            Console.Write(grid[(x, y)]);
+        else
+        if (d.Intersect([(x, y), (x, y - 1), (x, y - 2), (x, y - 3)]).Any())
+            Console.Write(grid[(x, y)]);
+        else
+        if (u.Intersect([(x, y), (x, y + 1), (x, y + 2), (x, y + 3)]).Any())
+            Console.Write(grid[(x, y)]);
+        else
+        if (dr.Intersect([(x, y), (x - 1, y - 1), (x - 2, y - 2), (x - 3, y - 3)]).Any())
+            Console.Write(grid[(x, y)]);
+        else
+        if (dl.Intersect([(x, y), (x + 1, y - 1), (x + 2, y - 2), (x + 3, y - 3)]).Any())
+            Console.Write(grid[(x, y)]);
+        else
+        if (ur.Intersect([(x, y), (x - 1, y + 1), (x - 2, y + 2), (x - 3, y + 3)]).Any())
+            Console.Write(grid[(x, y)]);
+        else
+        if (ul.Intersect([(x, y), (x + 1, y + 1), (x + 2, y + 2), (x + 3, y + 3)]).Any())
+            Console.Write(grid[(x, y)]);
+        else
+            Console.Write('.');
+    }
+    Console.WriteLine();
+}
+
+var result = r.Count + l.Count + d.Count + u.Count + dr.Count + dl.Count + ur.Count + ul.Count;
+
+Console.WriteLine(new { result });
+
+// 2466 is too low
