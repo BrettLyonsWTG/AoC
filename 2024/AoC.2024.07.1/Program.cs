04591bc23526819f4765d585520442c6e6798e0e
Brett Lyons <ultimojones@gmail.com>
2024-12-07 21:43:00 +1100

    🤠

---
+++ 2024/AoC.2024.07.1/Program.cs
@@ -0,0 +1,45 @@
+﻿var file = Debugger.IsAttached ? "example.txt" : "input.txt";
+
+long result = 0;
+
+foreach (var line in File.ReadAllLines(file))
+{
+    var value = long.Parse(line[..line.IndexOf(':')]);
+    List<long> numbers = [];
+    var numberSpan = line[(line.IndexOf(':') + 2)..].AsSpan();
+    foreach (var split in numberSpan.Split(' '))
+    {
+        numbers.Add(long.Parse(numberSpan[split]));
+    }
+
+    var combos = new List<List<char>>([['+'], ['*']]);
+    for (long i = 2; i < numbers.Count; i++)
+    {
+        combos = combos.Select(c => c.Append('+').ToList())
+            .Concat(combos.Select(c => c.Append('*').ToList())).ToList();
+    }
+
+    foreach (var combo in combos)
+    {
+        long calc = numbers[0];
+        for (int i = 0; i < combo.Count; i++)
+        {
+            if (combo[i] == '+')
+            {
+                calc += numbers[i + 1];
+            }
+            else
+            {
+                calc *= numbers[i + 1];
+            }
+        }
+        if (calc == value)
+        {
+            Console.WriteLine($"{line} > {string.Join(',', combo)}");
+            result += value;
+            break;
+        }
+    }
+}
+
+Console.WriteLine(new { result });
---
+++ 2024/AoC.2024.07.2/Program.cs
@@ -0,0 +1,45 @@
+﻿var file = Debugger.IsAttached ? "example.txt" : "input.txt";
+
+long result = 0;
+
+foreach (var line in File.ReadAllLines(file))
+{
+    var value = long.Parse(line[..line.IndexOf(':')]);
+    List<(long n, long p)> numbers = [];
+    var numberSpan = line[(line.IndexOf(':') + 2)..].AsSpan();
+    foreach (var split in numberSpan.Split(' '))
+    {
+        numbers.Add((long.Parse(numberSpan[split]), split.GetOffsetAndLength(numberSpan.Length).Length));
+    }
+
+    var combos = new List<List<char>>([['+'], ['*'], ['|']]);
+    for (long i = 2; i < numbers.Count; i++)
+    {
+        combos = combos.Select(c => c.Append('+').ToList())
+            .Concat(combos.Select(c => c.Append('*').ToList()))
+            .Concat(combos.Select(c => c.Append('|').ToList())).ToList();
+    }
+
+    foreach (var combo in combos)
+    {
+        long calc = numbers[0].n;
+        for (int i = 0; i < combo.Count; i++)
+        {
+            calc = combo[i] switch
+            {
+                '+' => calc + numbers[i + 1].n,
+                '*' => calc * numbers[i + 1].n,
+                '|' => calc * (long)Math.Pow(10, numbers[i + 1].p) + numbers[i + 1].n,
+                _ => throw new NotImplementedException()
+            };
+        }
+        if (calc == value)
+        {
+            Console.WriteLine($"{line} > {string.Join(',', combo)}");
+            result += value;
+            break;
+        }
+    }
+}
+
+Console.WriteLine(new { result });
