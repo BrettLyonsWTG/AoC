18138d240eee6ae9f2b3e91829e9f5fc385f72d3
Brett Lyons <ultimojones@gmail.com>
2024-12-12 07:52:32 +1100

    🤠

---
+++ 2024/AoC.2024.11.1/Program.cs
@@ -0,0 +1,38 @@
+﻿var file = Debugger.IsAttached ? "example.txt" : "input.txt";
+
+var stones = File.ReadAllText(file).Trim().Split().Select(long.Parse).ToList();
+
+if (Debugger.IsAttached) Console.WriteLine($"Initial arrangement:");
+if (Debugger.IsAttached) Console.WriteLine(string.Join(" ", stones));
+if (Debugger.IsAttached) Console.WriteLine();
+
+for (int b = 1; b <= (Debugger.IsAttached ? 6 : 75); b++)
+{
+    Console.WriteLine(new { b, stones.Count });
+    for (int s = 0; s < stones.Count; s++)
+    {
+        if (stones[s] == 0)
+        {
+            stones[s] = 1;
+            continue;
+        }
+        var digits = stones[s].ToString().Length;
+        if (digits % 2 == 0)
+        {
+            var before = stones[s];
+            var div = (long)Math.Pow(10, digits / 2);
+            var left = stones[s] / div;
+            var right = stones[s] % div;
+            stones[s] = left;
+            stones.Insert(++s, right);
+            continue;
+        }
+
+        stones[s] *= 2024;
+    }
+    if (Debugger.IsAttached) Console.WriteLine($"After {b} blinks:");
+    if (Debugger.IsAttached) Console.WriteLine(string.Join(" ", stones));
+    if (Debugger.IsAttached) Console.WriteLine();
+}
+
+Console.WriteLine(new { stones.Count });
---
+++ 2024/AoC.2024.11.2/Program.cs
@@ -0,0 +1,43 @@
+﻿var file = Debugger.IsAttached ? "example.txt" : "input.txt";
+var cycles = Debugger.IsAttached ? 6 : 75;
+
+var stones = File.ReadAllText(file).Trim().Split().Select(long.Parse).ToList();
+
+Console.WriteLine($"Initial arrangement:");
+Console.WriteLine(string.Join(" ", stones));
+Console.WriteLine();
+
+var splits = new Dictionary<long, long[]> { [0] = [1] };
+var nextKeys = stones.Except(splits.Keys).Distinct().ToList();
+
+for (int b = 1; b <= cycles && nextKeys.Count > 0; b++)
+{
+    var newKeys = new List<long>();
+    foreach (var s in nextKeys)
+    {
+        var digits = s.ToString().Length;
+        if (digits % 2 == 0)
+        {
+            var div = (long)Math.Pow(10, digits / 2);
+            var left = s / div;
+            var right = s % div;
+            splits[s] = [left, right];
+        }
+        else
+        {
+            splits[s] = [s * 2024];
+        }
+        newKeys.AddRange(splits[s].Except(splits.Keys));
+    }
+    nextKeys = newKeys;
+}
+
+var splitCounts = stones.GroupBy(s => s).ToFrozenDictionary(s => s.Key, s => s.LongCount());
+
+for (int c = 1; c <= cycles; c++)
+{
+    splitCounts = splitCounts.SelectMany(s => splits[s.Key].Select(x => new { x, s.Value })).GroupBy(n => n.x).ToFrozenDictionary(n => n.Key, n => n.Sum(x => x.Value));
+    Console.WriteLine(new { c, sum = splitCounts.Values.Sum() });
+}
+
+Console.WriteLine(new { sum = splitCounts.Values.Sum() });
