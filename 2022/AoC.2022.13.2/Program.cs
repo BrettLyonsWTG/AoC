be4033bd5eb446e2579f3f4dc56f282744b72c91
Brett Lyons <ultimojones@gmail.com>
2024-12-14 16:52:19 +1100

    🤠

--- 2022/AoC.2022.13.2/Program.cs
+++ 2022/AoC.2022.13.2/Program.cs
@@ -1,127 +1,113 @@
-﻿using ILoggerFactory logFactory = LoggerFactory.Create(builder => builder
-    .SetMinimumLevel(Debugger.IsAttached ? LogLevel.Debug : LogLevel.Information)
-    .AddSimpleConsole(options => options.SingleLine = true));
-ILogger log = logFactory.CreateLogger(typeof(Program).Assembly.GetName().Name!);
-string file = Debugger.IsAttached ? "example.txt" : "input.txt";
+﻿string file = Debugger.IsAttached ? "example.txt" : "input.txt";
 
-var lines = File.ReadAllLines(file);
-var packets = new List<(string left, string right)>();
-for (int i = 0; i < lines.Length; i += 3) { packets.Add((lines[i], lines[i + 1])); }
+var packets = File.ReadAllLines(file).Where(l => l.Length > 0).Select(l => new PacketData(l)).ToList();
 
-var result = packets.Select((p, i) =>
-{
-    var left = ParsePacket(p.left);
-    var right = ParsePacket(p.right);
-    log.LogDebug("== Pair {i} ==", i);
-    log.LogDebug("- Compare {left}", p.left);
-    log.LogDebug("       vs {right}", p.right);
-    var r = (i: i + 1, c: ComparePackets(left, right) is not 1);
-    log.LogDebug("");
-    return r;
-})
-.Where(p => p.c).Sum(p => p.i);
+var div1 = new PacketData("[[2]]");
+var div2 = new PacketData("[[6]]");
+packets.AddRange([div1, div2]);
 
-log.LogInformation("The sum of these indices is {result}.", result);
+packets.Sort();
+packets.ForEach(Console.WriteLine);
 
-int ComparePackets(PacketData left, PacketData right, int indent = 0)
-{
-    int result = 0;
+Console.WriteLine(new { d1 = packets.IndexOf(div1) + 1, d2 = packets.IndexOf(div2) + 1 });
+Console.WriteLine(new { key = (packets.IndexOf(div1) + 1L) * (packets.IndexOf(div2) + 1L) });
 
-    if (indent > 0)
-        log.LogDebug("{indent}- Compare {left} vs {right}", new string(' ', indent), left, right);
-    if (left.Value.HasValue && right.Value.HasValue)
-    {
-        result = Comparer<int>.Default.Compare(left.Value.Value, right.Value.Value);
-        if (result is -1)
-            log.LogDebug("{indent}- Left side is smaller, so inputs are in the right order", new string(' ', indent + 2));
-        else if (result is 1)
-            log.LogDebug("{indent}- Right side is smaller, so inputs are not in the right order", new string(' ', indent + 2));
-    }
-    else if (left.Value.HasValue)
-    {
-        log.LogDebug("{indent}- Mixed types; convert left to [{left}] and retry comparison", new string(' ', indent), left);
-        result = ComparePackets(new PacketData { List = [left] }, right, indent + 2);
-    }
-    else if (right.Value.HasValue)
-    {
-        log.LogDebug("{indent}- Mixed types; convert right to [{right}] and retry comparison", new string(' ', indent), right);
-        result = ComparePackets(left, new PacketData { List = [right] }, indent + 2);
-    }
-    else
-    {
-        int i = 0;
-        for (; i < Math.Min(left.List.Count, right.List.Count); i++)
-        {
-            result = ComparePackets(left.List[i], right.List[i], indent + 2);
-            if (result != 0)
-                break;
-        }
 
-        if (result == 0)
-        {
-            if (right.List.Count < left.List.Count)
-            {
-                result = 1;
-                log.LogDebug("{indent}- Right side ran out of items, so inputs are not in the right order", new string(' ', indent + 2));
-            }
-            else if (left.List.Count < right.List.Count)
-            {
-                result = -1;
-                log.LogDebug("{indent}- Left side ran out of items, so inputs are in the right order", new string(' ', indent + 2));
-            }
-        }
-    }
-    return result;
-}
-
-static PacketData ParsePacket(ReadOnlySpan<char> packet, bool sub = false)
-{
-    var result = new PacketData();
-
-    while (result.Size < packet.Length)
-    {
-        if (packet[result.Size] is ',')
-        {
-            result.Size++;
-        }
-        if (packet[result.Size] is '[')
-        {
-            result.Size++;
-            var subPacket = ParsePacket(packet[result.Size..], true);
-            if (!sub)
-            {
-                result = subPacket;
-            }
-            else
-            {
-                result.List.Add(subPacket);
-            }
-            result.Size += subPacket.Size;
-        }
-        else if (packet[result.Size] is ']')
-        {
-            result.Size++;
-            break;
-        }
-        else
-        {
-            var next = packet[result.Size..].IndexOfAnyExceptInRange('0', '9');
-            if (next > 0)
-            {
-                result.List.Add(new PacketData { Value = int.Parse(packet[result.Size..(result.Size + next)]), Size = next - result.Size });
-                result.Size += next;
-            }
-        }
-    }
-
-    return result;
-}
-
-class PacketData
+public class PacketData : IEquatable<PacketData>, IComparable<PacketData>
 {
     public int Size { get; set; } = 0;
     public int? Value { get; set; } = null;
     public List<PacketData> List { get; set; } = [];
+
+    public PacketData() { }
+
+    public PacketData(ReadOnlySpan<char> packet, bool sub = false)
+    {
+        while (Size < packet.Length)
+        {
+            if (packet[Size] is ',')
+            {
+                Size++;
+            }
+            if (packet[Size] is '[')
+            {
+                Size++;
+                var subPacket = new PacketData(packet[Size..], true);
+                if (!sub)
+                {
+                    Size = subPacket.Size;
+                    Value = subPacket.Value;
+                    List = subPacket.List;
+                }
+                else
+                {
+                    List.Add(subPacket);
+                }
+                Size += subPacket.Size;
+            }
+            else if (packet[Size] is ']')
+            {
+                Size++;
+                break;
+            }
+            else
+            {
+                var next = packet[Size..].IndexOfAnyExceptInRange('0', '9');
+                if (next > 0)
+                {
+                    List.Add(new PacketData { Value = int.Parse(packet[Size..(Size + next)]), Size = next - Size });
+                    Size += next;
+                }
+            }
+        }
+    }
+
+    public int CompareTo(PacketData? other)
+    {
+        if (other is null)
+            return 1;
+
+        int result = 0;
+
+        if (this.Value.HasValue && other.Value.HasValue)
+        {
+            result = Comparer<int>.Default.Compare(this.Value.Value, other.Value.Value);
+        }
+        else if (this.Value.HasValue)
+        {
+            result = new PacketData { List = [this] }.CompareTo(other);
+        }
+        else if (other.Value.HasValue)
+        {
+            result = this.CompareTo(new PacketData { List = [other] });
+        }
+        else
+        {
+            int i = 0;
+            for (; i < Math.Min(this.List.Count, other.List.Count); i++)
+            {
+                result = this.List[i].CompareTo(other.List[i]);
+                if (result != 0)
+                    break;
+            }
+
+            if (result == 0)
+            {
+                if (other.List.Count < this.List.Count)
+                {
+                    result = 1;
+                }
+                else if (this.List.Count < other.List.Count)
+                {
+                    result = -1;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public bool Equals(PacketData? other) => CompareTo(other) == 0;
 
     public override string ToString() => Value?.ToString() ?? $"[{string.Join(",", List)}]";
 }
---
+++ 2022/AoC.2024.14.1/Program.cs
@@ -0,0 +1,92 @@
+﻿string file = Debugger.IsAttached ? "example.txt" : "input.txt";
+
+var paths = File.ReadLines(file).Select(l => l.Split(" -> ").Select(p => { var s = p.Split(',').Select(int.Parse).ToArray(); return (x: s[0], y: s[1]); }).ToList()).ToList();
+
+var minx = paths.Min(p => p.Min(p => p.x)) - 1;
+var miny = 0;
+var maxx = paths.Max(p => p.Max(p => p.x)) + 1;
+var maxy = paths.Max(p => p.Max(p => p.y));
+Console.WriteLine(new { minx, maxx, miny, maxy });
+
+var rocks = new HashSet<(int x, int y)>();
+foreach (var path in paths)
+{
+    for (int i = 1; i < path.Count; i++)
+    {
+        if (path[i - 1].y == path[i].y)
+        {
+            var min = Math.Min(path[i - 1].x, path[i].x);
+            var max = Math.Max(path[i - 1].x, path[i].x);
+            for (int x = min; x <= max; x++)
+            {
+                rocks.Add((x, path[i].y));
+            }
+        }
+        else
+        {
+            var min = Math.Min(path[i - 1].y, path[i].y);
+            var max = Math.Max(path[i - 1].y, path[i].y);
+            for (int y = min; y <= max; y++)
+            {
+                rocks.Add((path[i].x, y));
+            }
+        }
+    }
+}
+
+var sand = (x: 500, y: 0);
+var stopped = new HashSet<(int x, int y)>();
+
+while (sand.y <= maxy)
+{
+    sand = (x: 500, y: 0);
+
+    while (sand.y <= maxy)
+    {
+        var down = (sand.x, sand.y + 1);
+        if (!rocks.Contains(down) && !stopped.Contains(down))
+        {
+            sand = down;
+            continue;
+        }
+        var downleft = (sand.x - 1, sand.y + 1);
+        if (!rocks.Contains(downleft) && !stopped.Contains(downleft))
+        {
+            sand = downleft;
+            continue;
+        }
+        var downright = (sand.x + 1, sand.y + 1);
+        if (!rocks.Contains(downright) && !stopped.Contains(downright))
+        {
+            sand = downright;
+            continue;
+        }
+        break;
+    }
+
+    if (sand.y <= maxy)
+        stopped.Add(sand);
+
+    //PrintGrid();
+    //Console.ReadLine();
+}
+
+PrintGrid();
+Console.WriteLine(new { stopped.Count });
+
+void PrintGrid()
+{
+    for (int y = miny; y <= maxy; y++)
+    {
+        for (int x = minx; x <= maxx; x++)
+        {
+            Console.Write(
+                rocks.Contains((x, y)) 
+                ? '#'
+                : stopped.Contains((x, y))
+                ? 'o'
+                : '.');
+        }
+        Console.WriteLine();
+    }
+}
