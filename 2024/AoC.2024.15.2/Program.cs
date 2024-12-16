3d6d81fa12c6903b4a4bbf5d987230dcedbd7fc4
Brett Lyons <ultimojones@gmail.com>
2024-12-16 11:00:41 +1100

    🤠

--- 2024/AoC.2024.15.2/Program.cs
+++ 2024/AoC.2024.15.2/Program.cs
@@ -14,14 +14,16 @@ var maxy = grid.Keys.Max(k => k.y);
 var robot = grid.Single(g => g.Value == '@').Key;
 grid[robot] = '.';
 
-void PrintGrid()
+void PrintGrid(List<(int x, int y)>? highlight = null, ConsoleColor color = ConsoleColor.White)
 {
     for (int y = 0; y <= maxy; y++)
     {
         for (int x = 0; x <= maxx; x++)
         {
             var p = (x, y);
+            if (highlight?.Contains(p) ?? false) Console.ForegroundColor = color;
             Console.Write(p == robot ? '@' : grid[p]);
+            Console.ForegroundColor = ConsoleColor.White;
         }
         Console.WriteLine();
     }
@@ -36,20 +38,20 @@ foreach (var m in moves)
 {
     var next = m switch
     {
-        '^' => (robot.x, robot.y - 1),
-        'v' => (robot.x, robot.y + 1),
-        '<' => (robot.x - 1, robot.y),
-        '>' => (robot.x + 1, robot.y),
+        '^' => (x: robot.x, y: robot.y - 1),
+        'v' => (x: robot.x, y: robot.y + 1),
+        '<' => (x: robot.x - 1, y: robot.y),
+        '>' => (x: robot.x + 1, y: robot.y),
         _ => throw new InvalidOperationException()
     };
 
     if (grid[next] is not '#')
     {
-        if (grid[next] is not '[' or ']')
+        if (grid[next] is not ('[' or ']'))
         {
             robot = next;
         }
-        else if (grid[next] is '[' or ']' && m is '<' or '>')
+        else if (m is ('<' or '>'))
         {
             var space = (m switch
             {
@@ -67,15 +69,43 @@ foreach (var m in moves)
                 robot = next;
             }
         }
-    }
+        else if (m is ('^' or 'v'))
+        {
+            List<(int x, int y)> nexts = [next, (grid[next] is '[' ? next.x + 1 : next.x - 1, next.y)];
+            List<(int x, int y)> pushing = [];
+            var dir = m is '^' ? -1 : 1;
 
-    if (Debugger.IsAttached) Console.WriteLine(m);
-    if (Debugger.IsAttached) PrintGrid();
-    //if (Debugger.IsAttached) Console.ReadLine();
+            while (nexts.Count > 0 && !nexts.Any(n => grid[n] is '#'))
+            {
+                pushing.AddRange(nexts);
+                nexts = nexts.Select(n => (n.x, y: n.y + dir))
+                    .SelectMany(n => ((int x, int y)[])(grid[n] switch
+                    {
+                        '[' => [n, (n.x + 1, n.y)],
+                        ']' => [(n.x - 1, n.y), n],
+                        '#' => [n],
+                        _ => []
+                    })).Distinct().ToList();
+            }
+
+            if (nexts.Count is 0)
+            {
+                if (Debugger.IsAttached) Console.WriteLine(m);
+                if (Debugger.IsAttached) PrintGrid(pushing, ConsoleColor.Green);
+                var vals = pushing.Join(grid, p => p, g => g.Key, (p, g) => g).ToDictionary();
+                pushing.ForEach(p => grid[p] = '.');
+                pushing.ForEach(p => grid[(p.x, p.y + dir)] = vals[p]);
+                var pushed = pushing.Select(p => (p.x, p.y + dir)).ToList();
+                robot = next;
+                if (Debugger.IsAttached) PrintGrid(pushed, ConsoleColor.Red);
+                if (Debugger.IsAttached) Debugger.Break();
+            }
+        }
+    }
 }
 
 PrintGrid();
 
-//var result = grid.Where(g => g.Value is 'O').Sum(g => g.Key.x + g.Key.y * 100);
+var result = grid.Where(g => g.Value is '[').Sum(g => g.Key.x + g.Key.y * 100);
 
-//Console.WriteLine(new { result });
+Console.WriteLine(new { result });
