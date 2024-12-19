6a96c50337e2e63c1118edd6cf46c8a0b96d1cda
Brett Lyons <ultimojones@gmail.com>
2024-12-20 10:40:18 +1100

    🤠

--- 2024/AoC.2024.17.2/Program.cs
+++ 2024/AoC.2024.17.2/Program.cs
@@ -2,25 +2,36 @@
 {
     regb = rega % 8;
     regb ^= 1;
-    regc = rega / 32;
-    regc ^= regb;
+    regc = (ulong)(rega / Math.Pow(2, regb));
+    regb ^= regc;
     rega /= 8;
     regb ^= 4;
     outval = (uint)(regb % 8);
     return rega is 0;
 }
 
-ulong rega = (ulong)(6 * Math.Pow(8, 8));
+ulong rega = 5;
 ulong regb = 0;
 ulong regc = 0;
 
-var outputs = new List<uint>();
+//uint[] targets = [7, 3, 0, 5, 7, 1, 4, 0, 5];
+//for (int t = targets.Length - 1; t >= 0; t--)
+//{
+//    ReverseInvoke(targets[t], ref rega, ref regb, ref regc);
+//    Console.WriteLine(new { t = targets[t], rega, regb, regc });
+//}
 
+//Console.WriteLine(new { rega });
+
+var outputs = new List<uint>();
 bool ended = false;
+rega = 28066687;
+regb = 0;
+regc = 0;
 while (!ended)
 {
     ended = Invoke(ref rega, ref regb, ref regc, out uint outval);
+    Console.WriteLine(new { t = outval, rega, regb, regc });
     outputs.Add(outval);
 }
-
 Console.WriteLine(string.Join(',', outputs));
