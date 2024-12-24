3bbbab8f41bafcfb5e70ba129c7dbd71283e61b1
Brett Lyons <Brett.Lyons@wisetechglobal.com>
2024-12-24 16:01:10 +1100

    🤠

--- 2024/AoC.2024.19.2/Program.cs
+++ 2024/AoC.2024.19.2/Program.cs
@@ -1,24 +1,41 @@
-﻿var file = Debugger.IsAttached ? "input.txt" : "input.txt";
+﻿using System.ComponentModel.DataAnnotations;
+
+var file = Debugger.IsAttached ? "example.txt" : "input.txt";
 
 var lines = File.ReadAllLines(file);
 
 var towels = lines[0].Split(", ");
 var designs = lines[2..].ToArray();
 
-var possible = 0;
+long possible = 0;
 
 foreach (var design in designs)
 {
     Console.WriteLine(new { design });
-    List<string> patterns = [""];
-    while (patterns.Count > 0)
+    Dictionary<int, long> combos = new() { [0] = 1 };
+    while (combos.Count > 0)
     {
-        patterns = patterns.SelectMany(p => 
-            towels.Where(t => design.Substring(p.Length).StartsWith(t))
-                .Select(t => p + t)).ToList();
+        var mincombo = combos.Keys.Min();
+        var unmatched = design[mincombo..];
+        var matches = towels.Where(unmatched.StartsWith).Select(t => t.Length).GroupBy(l => l).Select(g => (length: g.Key, count: g.Count())).ToList();
 
-        possible += patterns.Count(n => n == design);
-        patterns.RemoveAll(n => n == design);
+        foreach (var match in matches)
+        {
+            var next = mincombo + match.length;
+            if (next == design.Length)
+            {
+                possible += combos[mincombo] * match.count;
+            }
+            else
+            {
+                if (!combos.ContainsKey(next))
+                {
+                    combos[next] = 0;
+                }
+                combos[next] += combos[mincombo] * match.count;
+            }
+        }
+        combos.Remove(mincombo);
     }
 }
 
