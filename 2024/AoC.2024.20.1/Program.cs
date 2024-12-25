95ec8cff2f3ac14d91a92bf188a0e886576deb93
Brett Lyons <ultimojones@gmail.com>
2024-12-25 14:52:12 +1100

    🤠

--- 2024/AoC.2024.20.1/Program.cs
+++ 2024/AoC.2024.20.1/Program.cs
@@ -14,55 +14,28 @@ var walls = map.Where(m => m.Value == '#').Select(m => m.Key).ToHashSet();
 var maxx = map.Keys.Max(p => p.x);
 var maxy = map.Keys.Max(p => p.y);
 
-var cuts = map.Where(m => 
-    (m.Key.x > 1 && m.Key.x < maxx - 1 && m.Key.y > 1 && m.Key.y < maxy - 1 && m.Value is '#')
-    && ((map[(m.Key.x - 1, m.Key.y)] == '.' && map[(m.Key.x + 1, m.Key.y)] == '.')
-        || (map[(m.Key.x, m.Key.y - 1)] == '.' && map[(m.Key.x, m.Key.y - 1)] == '.')))
-    .Select(m => m.Key);
-
-void PrintMap(List<(int, int)> path)
+void PrintMap(List<(int, int)> path, (int, int) cut = default)
 {
     for (int y = 0; y <= maxy; y++)
     {
         for (int x = 0; x <= maxx; x++)
         {
+            Console.ForegroundColor = (x, y) == cut ? ConsoleColor.Green : path.Contains((x, y)) ? ConsoleColor.Red : ConsoleColor.White;
+            Console.BackgroundColor = (x, y) == cut ? ConsoleColor.White : ConsoleColor.Black;
             Console.Write((x, y) == start ? 'S' : (x, y) == end ? 'E' : path.Contains((x, y)) ? 'O' : map[(x, y)]);
+            Console.ResetColor();
         }
         Console.WriteLine();
     }
 }
 
-//int steps = 0;
-//var current = new List<(int x, int y)> { start };
-//var done = new List<(int x, int y)>();
-//var moves = new List<((int x, int y), (int x, int y))>();
-
-//while (current.Count > 0)
-//{
-//    steps++;
-//    var nextMoves = current.SelectMany(c => new[] { (c.x, c.y - 1), (c.x, c.y + 1), (c.x - 1, c.y), (c.x + 1, c.y) }
-//        .Except(walls)
-//        .Except(done)
-//        .Select(n => (from: c, to: n)))
-//        .ToArray();
-//    moves.AddRange(nextMoves);
-//    var nexts = nextMoves.Select(n => n.to).Distinct().ToList();
-//    if (nexts.Contains(end)) break;
-//    done.AddRange(nexts);
-//    current = nexts;
-//}
-
-//PrintMap(current);
-//Console.WriteLine(new { steps });
-//Console.WriteLine();
-
-int GetSteps((int x, int y) cut)
+List<(int x, int y)> GetSteps((int x, int y) cut)
 {
     var cutWalls = walls.Except([cut]).ToHashSet();
     int steps = 0;
     var current = new List<(int x, int y)> { start };
     var done = new List<(int x, int y)>();
-    var moves = new List<((int x, int y), (int x, int y))>();
+    var moves = new List<((int x, int y) from, (int x, int y) to)>();
 
     while (current.Count > 0)
     {
@@ -79,10 +52,42 @@ int GetSteps((int x, int y) cut)
         current = nexts;
     }
 
-    return steps;
+    var path = new List<(int x, int y)> { end };
+    while (path.Last() != start)
+    {
+        var last = path.Last();
+        var move = moves.First(m => m.to == last);
+        path.Add(move.from);
+    }
+    return path;
 }
 
-var full = GetSteps((0, 0));
+var full = GetSteps((-1, -1));
+PrintMap(full, (-1, -1));
+Console.WriteLine();
 
-var savings = cuts.Select(c => full - GetSteps(c)).GroupBy(c => c).Select(c => (saving: c.Key, count: c.Count())).OrderBy(c => c.saving).ToList();
-savings.ForEach(s => Console.WriteLine(s));
+var cuts = map.Where(m => m.Value is '#' &&
+    ((m.Key.x > 1 && m.Key.x < maxx - 1 && map[(m.Key.x - 1, m.Key.y)] == '.' && map[(m.Key.x + 1, m.Key.y)] == '.') ||
+     (m.Key.y > 1 && m.Key.y < maxy - 1 && map[(m.Key.x, m.Key.y - 1)] == '.' && map[(m.Key.x, m.Key.y + 1)] == '.')))
+    .Select(m => m.Key)
+    .OrderBy(m => m.y)
+    .ThenBy(m => m.x);
+
+var savingCount = 0;
+var savings = cuts.Select(cut => (cut, path: GetSteps(cut))).Where(c => full.Count - c.path.Count >= 100);
+foreach (var (cut, path) in savings)
+{
+    savingCount++;
+    //PrintMap(path, cut);
+    Console.WriteLine(new { cut, savings = full.Count - path.Count });
+    //Console.WriteLine();
+    //Console.ReadLine();
+}
+
+//foreach (var cut in cuts)
+//{
+//    var steps = GetSteps(cut);
+//    PrintMap(steps, cut);
+//    Console.WriteLine(new { cut, steps = steps.Count - 1 });
+//    Console.ReadLine();
+//}
