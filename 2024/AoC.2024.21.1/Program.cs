df652b59af1989e0d326df1477508504ea59cf11
Brett Lyons <ultimojones@gmail.com>
2025-01-04 22:24:36 +1100

    🤠

--- 2024/AoC.2024.21.1/Program.cs
+++ 2024/AoC.2024.21.1/Program.cs
@@ -2,7 +2,25 @@
 
 var codes = File.ReadAllLines(file).Select(c => (code: c, num: int.Parse(c[..3]))).ToList();
 
-List<List<string>> GetNumPresses(char button, ref (int x, int y) pos)
+IEnumerable<List<char>> GetNumCombos(IEnumerable<char> input, IEnumerable<char> done, (int x, int y) pos)
+{
+    if (input.Any())
+    {
+        foreach (var presses in GetNumPresses(input.First(), ref pos))
+        {
+            foreach (var combo in GetNumCombos(input.Skip(1), done.Concat(presses).Append('A'), pos))
+            {
+                yield return combo;
+            }
+        }
+    }
+    else
+    {
+        yield return done.ToList();
+    }
+}
+
+List<List<char>> GetNumPresses(char button, ref (int x, int y) pos)
 {
     var presses = new List<List<string>>();
 
@@ -24,22 +42,42 @@ List<List<string>> GetNumPresses(char button, ref (int x, int y) pos)
     var presses1 = new List<char>();
     if (next.x > pos.x) presses1.AddRange(Enumerable.Repeat('>', next.x - pos.x));
     if (next.x < pos.x) presses1.AddRange(Enumerable.Repeat('<', pos.x - next.x));
-    if (next.y > pos.y) presses1.AddRange(Enumerable.Repeat('^', next.y - pos.y));
-    if (next.y < pos.y) presses1.AddRange(Enumerable.Repeat('v', pos.y - next.y));
+    if (next.y > pos.y) presses1.AddRange(Enumerable.Repeat('v', next.y - pos.y));
+    if (next.y < pos.y) presses1.AddRange(Enumerable.Repeat('^', pos.y - next.y));
 
     var combos = GetCombos(presses1).ToList();
 
-    foreach (var combo in combos)
+    if (pos.x == 0 && next.y == 3)
     {
-        //presses.Add([new([.. combo])]);
-        var presses1
-        foreach (var dirPresses in GetDirPresses(button, ref pos))
-        {
-            presses.Add(dirPresses.Concat(combo).ToList());
-        }
+        var down = 3 - pos.y;
+        combos.RemoveAll(c => c.Take(down).All(d => d == 'v'));
+    }
+    else if (pos.y == 3 && next.x == 0)
+    {
+        var left = 2 - pos.x;
+        combos.RemoveAll(c => c.First() == '<');
     }
 
-    return  presses;
+    pos = next;
+    return combos;
+}
+
+IEnumerable<List<char>> GetDirCombos(IEnumerable<char> input, IEnumerable<char> done, (int x, int y) pos)
+{
+    if (input.Any())
+    {
+        foreach (var presses in GetDirPresses(input.First(), ref pos))
+        {
+            foreach (var combo in GetDirCombos(input.Skip(1), done.Concat(presses).Append('A'), pos))
+            {
+                yield return combo;
+            }
+        }
+    }
+    else
+    {
+        yield return done.ToList();
+    }
 }
 
 List<List<char>> GetDirPresses(char button, ref (int x, int y) pos)
@@ -58,10 +96,22 @@ List<List<char>> GetDirPresses(char button, ref (int x, int y) pos)
     var presses1 = new List<char>();
     if (next.x > pos.x) presses1.AddRange(Enumerable.Repeat('>', next.x - pos.x));
     if (next.x < pos.x) presses1.AddRange(Enumerable.Repeat('<', pos.x - next.x));
-    if (next.y > pos.y) presses1.AddRange(Enumerable.Repeat('^', next.y - pos.y));
-    if (next.y < pos.y) presses1.AddRange(Enumerable.Repeat('v', pos.y - next.y));
+    if (next.y > pos.y) presses1.AddRange(Enumerable.Repeat('v', next.y - pos.y));
+    if (next.y < pos.y) presses1.AddRange(Enumerable.Repeat('^', pos.y - next.y));
 
-    return GetCombos(presses1).ToList();
+    var combos = GetCombos(presses1).ToList();
+
+    if (button == '<' && pos.y == 0)
+    {
+        combos.RemoveAll(c => c.Last() == 'v');
+    }
+    else if (pos == (0, 1) && next.y == 0)
+    {
+        combos.RemoveAll(c => c.First() == '^');
+    }
+
+    pos = next;
+    return combos;
 }
 
 List<List<char>> GetCombos(List<char> input)
@@ -90,224 +140,52 @@ IEnumerable<List<int>> GetComboIds(IEnumerable<int> input, IEnumerable<int> done
     }
 }
 
-var pos = (2, 3);
-var presses = GetNumPresses('7', ref pos);
+string GetPresses(string code)
+{
+    string bestPresses = "";
+    var best1 = int.MaxValue;
+    var best2 = int.MaxValue;
 
+    var numCombos = GetNumCombos(code, [], (2, 3)).ToList();
+    foreach (var combo in numCombos)
+    {
+        var combos1 = GetDirCombos(combo, [], (2, 0)).ToList();
+        var min1 = combos1.Min(c => c.Count);
+        if (min1 <= best1)
+        {
+            best1 = min1;
+            foreach (var combo1 in combos1.Where(c => c.Count == min1))
+            {
+                var combos2 = GetDirCombos(combo1, [], (2, 0)).ToList();
+                var min2 = combos2.Min(c => c.Count);
+                if (min2 <= best2)
+                {
+                    best2 = min2;
+                    bestPresses = new([.. combos2.First(c => c.Count == min2)]);
+                }
+            }
+        }
+    }
 
-//var presses = GetNumPresses('3', ref pos);
+    return bestPresses;
+}
 
-//var combos = GetCombos(['1', '2', '3', '4'], []).ToList();
+long total = 0;
+foreach (var line in File.ReadLines(file))
+{
+    var presses = GetPresses(line);
+    var num = int.Parse(line[..3]);
+    Console.WriteLine($"{line}: {presses}");
+    total += presses.Length * num;
+}
 
-//foreach (var combo in combos)
-//{
-//    Console.WriteLine(string.Join(", ", combo));
-//}
+Console.WriteLine(total);
 
-
-//long complexity = 0;
-
-//foreach (var (code, num) in codes)
-//{
-//    var pos = (x: 2, y: 3);
-//    var presses = new List<char>();
-
-//    for (var i = 0; i < 4; i++)
-//    {
-//        (int x, int y) next = code[i] switch
-//        {
-//            'A' => (2, 3),
-//            '0' => (1, 3),
-//            '1' => (0, 2),
-//            '2' => (1, 2),
-//            '3' => (2, 2),
-//            '4' => (0, 1),
-//            '5' => (1, 1),
-//            '6' => (2, 1),
-//            '7' => (0, 0),
-//            '8' => (1, 0),
-//            '9' => (2, 0),
-//            _ => throw new InvalidOperationException()
-//        };
-
-//        while (pos != next)
-//        {
-//            var press = (next.x - pos.x, next.y - pos.y) switch
-//            {
-//                ( > 0, _) => '>',
-//                (_, < 0) => '^',
-//                (_, > 0) when pos.x > 0 || pos.y < 2 => 'v',
-//                ( < 0, _) when pos.x > 1 || pos.y < 3 => '<',
-//                _ => throw new InvalidOperationException()
-//            };
-//            pos = press switch
-//            {
-//                '^' => (pos.x, pos.y - 1),
-//                '<' => (pos.x - 1, pos.y),
-//                'v' => (pos.x, pos.y + 1),
-//                '>' => (pos.x + 1, pos.y),
-//                _ => throw new InvalidOperationException()
-//            };
-//            presses.Add(press);
-//        }
-
-//        presses.Add('A');
-//    }
-
-//    var nextPresses = GetForDirKeypad(presses);
-//    var lastPresses = GetForDirKeypad(nextPresses);
-
-//    Console.WriteLine(new string([.. lastPresses]));
-//    Console.WriteLine(new string([.. nextPresses]));
-//    Console.WriteLine(new string([.. presses]));
-//    Console.WriteLine($"{code}: {new string([.. lastPresses])}");
-//    Console.WriteLine();
-
-//    complexity += lastPresses.Count * num;
-//}
-
-//Console.WriteLine(new { complexity });
-
-//List<char> GetForDirKeypad(List<char> inPresses)
-//{
-//    var pos = (x: 2, y: 0);
-//    var outPresses = new List<char>();
-
-//    for (var i = 0; i < inPresses.Count; i++)
-//    {
-//        (int x, int y) next = inPresses[i] switch
-//        {
-//            'A' => (2, 0),
-//            '^' => (1, 0),
-//            '<' => (0, 1),
-//            'v' => (1, 1),
-//            '>' => (2, 1),
-//            _ => throw new InvalidOperationException()
-//        };
-
-//        while (pos != next)
-//        {
-//            var press = (next.x - pos.x, next.y - pos.y) switch
-//            {
-//                ( > 0, _) => '>',
-//                (_, < 0) when pos.x > 0 => '^',
-//                (_, > 0) => 'v',
-//                ( < 0, _) => '<',
-//                _ => throw new InvalidOperationException()
-//            };
-//            pos = press switch
-//            {
-//                '^' => (pos.x, pos.y - 1),
-//                '<' => (pos.x - 1, pos.y),
-//                'v' => (pos.x, pos.y + 1),
-//                '>' => (pos.x + 1, pos.y),
-//                _ => throw new InvalidOperationException()
-//            };
-//            outPresses.Add(press);
-//        }
-
-//        outPresses.Add('A');
-//    }
-
-//    return outPresses;
-//}
-
-//string ReverseForDirKeypad(ReadOnlySpan<char> inPresses)
-//{
-//    var pos = (x: 2, y: 0);
-//    var outPresses = new List<char>();
-
-//    for (var i = 0; i < inPresses.Length; i++)
-//    {
-//        if (inPresses[i] == 'A')
-//        {
-//            outPresses.Add(pos switch
-//            {
-//                (2, 0) => 'A',
-//                (1, 0) => '^',
-//                (0, 1) => '<',
-//                (1, 1) => 'v',
-//                (2, 1) => '>',
-//                _ => throw new InvalidOperationException()
-//            });
-//            continue;
-//        }
-//        else
-//        {
-//            pos = inPresses[i] switch
-//            {
-//                '^' => (pos.x, pos.y - 1),
-//                '<' => (pos.x - 1, pos.y),
-//                'v' => (pos.x, pos.y + 1),
-//                '>' => (pos.x + 1, pos.y),
-//                _ => throw new InvalidOperationException()
-//            };
-//        }
-//    }
-
-//    return new string(outPresses.ToArray());
-//}
-
-//string ReverseForNumKeypad(ReadOnlySpan<char> inPresses)
-//{
-//    var pos = (x: 2, y: 3);
-//    var outPresses = new List<char>();
-
-//    for (var i = 0; i < inPresses.Length; i++)
-//    {
-//        if (inPresses[i] == 'A')
-//        {
-//            outPresses.Add(pos switch
-//            {
-//                (2, 3) => 'A',
-//                (1, 3) => '0',
-//                (0, 2) => '1',
-//                (1, 2) => '2',
-//                (2, 2) => '3',
-//                (0, 1) => '4',
-//                (1, 1) => '5',
-//                (2, 1) => '6',
-//                (0, 0) => '7',
-//                (1, 0) => '8',
-//                (2, 0) => '9',
-//                _ => throw new InvalidOperationException()
-//            });
-//            continue;
-//        }
-//        else
-//        {
-//            pos = inPresses[i] switch
-//            {
-//                '^' => (pos.x, pos.y - 1),
-//                '<' => (pos.x - 1, pos.y),
-//                'v' => (pos.x, pos.y + 1),
-//                '>' => (pos.x + 1, pos.y),
-//                _ => throw new InvalidOperationException()
-//            };
-//        }
-//    }
-
-//    return new string(outPresses.ToArray());
-//}
-
-////{
-////    var inPresses = "v<<A>>^AvA^Av<<A>>^AAv<A<A>>^AAvAA<^A>Av<A>^AA<A>Av<A<A>>^AAAvA<^A>A";
-////    Console.WriteLine(inPresses);
-////    var presses1 = ReverseForDirKeypad(inPresses);
-////    Console.WriteLine(presses1);
-////    var presses2 = ReverseForDirKeypad(presses1);
-////    Console.WriteLine(presses2);
-////    var presses3 = ReverseForNumKeypad(presses2);
-////    Console.WriteLine(presses3);
-////}
-////{
-////    var inPresses = "<v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A";
-////    Console.WriteLine(inPresses);
-////    var presses1 = ReverseForDirKeypad(inPresses);
-////    Console.WriteLine(presses1);
-////    var presses2 = ReverseForDirKeypad(presses1);
-////    Console.WriteLine(presses2);
-////    var presses3 = ReverseForNumKeypad(presses2);
-////    Console.WriteLine(presses3);
-////}
-//// 253278 is too high
-//// 242974 is too high
+/*
+826A: <vA<AA>>^AvA<^A>AAAvA^A<vA<A>>^AA<A>vA^A<v<A>>^A<vA>A^A<A>A<vA<A>>^AA<A>vA^A
+341A: <v<A>>^AvA^A<vA<AA>>^AAvA<^A>AvA^A<vA<A>>^A<A>vA^A<vA>^A<v<A>>^AvA^A<A>A
+582A: <vA<AA>>^AvA<^A>AAvA^A<v<A>>^AvA^A<vA<A>>^AA<A>vA^A<vA<A>>^AvA^A<A>A
+983A: <v<A>>^AAAvA^A<vA<AA>>^AvAA<^A>A<vA<A>>^AAvA^A<A>A<vA<A>>^A<A>vA^A
+670A: <v<A>>^AAvA^A<vA<AA>>^AAvA<^A>AvA^A<vA>^A<v<A>>^AAA<A>vA^A<vA>^A<A>A
+237342
+*/
