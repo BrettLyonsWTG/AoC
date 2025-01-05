b38b5b1ecd87c9ba73946374ca5889c60a164818
Brett Lyons <ultimojones@gmail.com>
2025-01-05 17:07:54 +1100

    🤠

--- 2024/AoC.2024.21.2/Program.cs
+++ 2024/AoC.2024.21.2/Program.cs
@@ -1,173 +1,99 @@
-﻿var file = Debugger.IsAttached ? "example.txt" : "input.txt";
+﻿using System.Runtime.InteropServices;
+
+var file = Debugger.IsAttached ? "example.txt" : "input.txt";
 
 var codes = File.ReadAllLines(file).Select(c => (code: c, num: int.Parse(c[..3]))).ToList();
 
-IEnumerable<List<char>> GetNumCombos(IEnumerable<char> input, IEnumerable<char> done, (int x, int y) pos)
+static (int x, int y) GetNumPos(char button) => button switch
 {
-    if (input.Any())
+    'A' => (2, 3),
+    '0' => (1, 3),
+    '1' => (0, 2),
+    '2' => (1, 2),
+    '3' => (2, 2),
+    '4' => (0, 1),
+    '5' => (1, 1),
+    '6' => (2, 1),
+    '7' => (0, 0),
+    '8' => (1, 0),
+    '9' => (2, 0),
+    _ => throw new InvalidOperationException()
+};
+
+static (int x, int y) GetDirPos(byte button) => (DirPad)button switch
+{
+    DirPad.Enter => (2, 0),
+    DirPad.Up => (1, 0),
+    DirPad.Left => (0, 1),
+    DirPad.Down => (1, 1),
+    DirPad.Right => (2, 1),
+    _ => throw new InvalidOperationException()
+};
+
+static IEnumerable<byte> GetNumPresses(char button, ref (int x, int y) pos)
+{
+    var next = GetNumPos(button);
+
+    var presses = Enumerable.Empty<byte>();
+
+    if (pos.y == 3 && next.x == 0)
     {
-        foreach (var presses in GetNumPresses(input.First(), ref pos))
-        {
-            foreach (var combo in GetNumCombos(input.Skip(1), done.Concat(presses).Append('A'), pos))
-            {
-                yield return combo;
-            }
-        }
+        presses = presses.Concat(Enumerable.Repeat((byte)DirPad.Up, pos.y - next.y));
+        presses = presses.Concat(Enumerable.Repeat((byte)DirPad.Left, pos.x - next.x));
+    }
+    else if (pos.x == 0 && next.y == 3)
+    {
+        presses = presses.Concat(Enumerable.Repeat((byte)DirPad.Right, next.x - pos.x));
+        presses = presses.Concat(Enumerable.Repeat((byte)DirPad.Down, next.y - pos.y));
     }
     else
     {
-        yield return done.ToList();
+        if (next.x < pos.x) presses = presses.Concat(Enumerable.Repeat((byte)DirPad.Left, pos.x - next.x));
+        if (next.y < pos.y) presses = presses.Concat(Enumerable.Repeat((byte)DirPad.Up, pos.y - next.y));
+        if (next.y > pos.y) presses = presses.Concat(Enumerable.Repeat((byte)DirPad.Down, next.y - pos.y));
+        if (next.x > pos.x) presses = presses.Concat(Enumerable.Repeat((byte)DirPad.Right, next.x - pos.x));
     }
-}
-
-List<List<char>> GetNumPresses(char button, ref (int x, int y) pos)
-{
-    var presses = new List<List<string>>();
-
-    (int x, int y) next = button switch
-    {
-        'A' => (2, 3),
-        '0' => (1, 3),
-        '1' => (0, 2),
-        '2' => (1, 2),
-        '3' => (2, 2),
-        '4' => (0, 1),
-        '5' => (1, 1),
-        '6' => (2, 1),
-        '7' => (0, 0),
-        '8' => (1, 0),
-        '9' => (2, 0),
-        _ => throw new InvalidOperationException()
-    };
-    var presses1 = new List<char>();
-    if (next.x > pos.x) presses1.AddRange(Enumerable.Repeat('>', next.x - pos.x));
-    if (next.x < pos.x) presses1.AddRange(Enumerable.Repeat('<', pos.x - next.x));
-    if (next.y > pos.y) presses1.AddRange(Enumerable.Repeat('v', next.y - pos.y));
-    if (next.y < pos.y) presses1.AddRange(Enumerable.Repeat('^', pos.y - next.y));
-
-    var combos = GetCombos(presses1).ToList();
-
-    if (pos.x == 0 && next.y == 3)
-    {
-        var down = 3 - pos.y;
-        combos.RemoveAll(c => c.Take(down).All(d => d == 'v'));
-    }
-    else if (pos.y == 3 && next.x == 0)
-    {
-        var left = 2 - pos.x;
-        combos.RemoveAll(c => c.First() == '<');
-    }
+    presses = presses.Append((byte)DirPad.Enter);
 
     pos = next;
-    return combos;
+    return presses;
 }
 
-IEnumerable<List<char>> GetDirCombos(IEnumerable<char> input, IEnumerable<char> done, (int x, int y) pos)
+IEnumerable<byte> GetDirPresses(byte button, ref (int x, int y) pos)
 {
-    if (input.Any())
-    {
-        foreach (var presses in GetDirPresses(input.First(), ref pos))
-        {
-            foreach (var combo in GetDirCombos(input.Skip(1), done.Concat(presses).Append('A'), pos))
-            {
-                yield return combo;
-            }
-        }
-    }
-    else
-    {
-        yield return done.ToList();
-    }
-}
+    var next = GetDirPos(button);
 
-List<List<char>> GetDirPresses(char button, ref (int x, int y) pos)
-{
-    var presses = new List<List<string>>();
-
-    (int x, int y) next = button switch
-    {
-        'A' => (2, 0),
-        '^' => (1, 0),
-        '<' => (0, 1),
-        'v' => (1, 1),
-        '>' => (2, 1),
-        _ => throw new InvalidOperationException()
-    };
-    var presses1 = new List<char>();
-    if (next.x > pos.x) presses1.AddRange(Enumerable.Repeat('>', next.x - pos.x));
-    if (next.x < pos.x) presses1.AddRange(Enumerable.Repeat('<', pos.x - next.x));
-    if (next.y > pos.y) presses1.AddRange(Enumerable.Repeat('v', next.y - pos.y));
-    if (next.y < pos.y) presses1.AddRange(Enumerable.Repeat('^', pos.y - next.y));
-
-    var combos = GetCombos(presses1).ToList();
-
-    if (button == '<' && pos.y == 0)
-    {
-        combos.RemoveAll(c => c.Last() == 'v');
-    }
-    else if (pos == (0, 1) && next.y == 0)
-    {
-        combos.RemoveAll(c => c.First() == '^');
-    }
+    var presses = Enumerable.Empty<byte>();
+    if (next.y > pos.y) presses = presses.Concat(Enumerable.Repeat((byte)DirPad.Down, next.y - pos.y));
+    if (next.x < pos.x) presses = presses.Concat(Enumerable.Repeat((byte)DirPad.Left, pos.x - next.x));
+    if (next.x > pos.x) presses = presses.Concat(Enumerable.Repeat((byte)DirPad.Right, next.x - pos.x));
+    if (next.y < pos.y) presses = presses.Concat(Enumerable.Repeat((byte)DirPad.Up, pos.y - next.y));
+    presses = presses.Append((byte)DirPad.Enter);
 
     pos = next;
-    return combos;
+    return presses;
 }
 
-List<List<char>> GetCombos(List<char> input)
+long GetPresses(string code)
 {
-    var comboIds = GetComboIds(Enumerable.Range(0, input.Count), []).ToList();
-    var combos = comboIds.Select(c => c.Select(d => input[d]).ToList()).DistinctBy(c => new string([.. c])).ToList();
-    return combos;
-}
+    var pos = GetNumPos('A');
+    var presses = code.SelectMany(c => GetNumPresses(c, ref pos)).ToArray();
 
-IEnumerable<List<int>> GetComboIds(IEnumerable<int> input, IEnumerable<int> done)
-{
-    bool noInput = true;
-
-    foreach (var i in input)
+    for (int i = 0; i < 25; i++)
     {
-        noInput = false;
-        foreach (var combo in GetComboIds(input.Except([i]), done.Append(i)))
+        GC.Collect();
+        pos = GetDirPos((byte)DirPad.Enter);
+        using var mem = new MemoryStream();
+        foreach (var press in presses)
         {
-            yield return combo;
+            var dirPresses = GetDirPresses(press, ref pos);
+            mem.Write(dirPresses.ToArray());
         }
+        presses = mem.ToArray();
+        Console.WriteLine($"{code}: {i + 1}={presses.Count()}");
     }
 
-    if (noInput)
-    {
-        yield return done.ToList();
-    }
-}
-
-string GetPresses(string code)
-{
-    string bestPresses = "";
-    var best1 = int.MaxValue;
-    var best2 = int.MaxValue;
-
-    var numCombos = GetNumCombos(code, [], (2, 3)).ToList();
-    foreach (var combo in numCombos)
-    {
-        var combos1 = GetDirCombos(combo, [], (2, 0)).ToList();
-        var min1 = combos1.Min(c => c.Count);
-        if (min1 <= best1)
-        {
-            best1 = min1;
-            foreach (var combo1 in combos1.Where(c => c.Count == min1))
-            {
-                var combos2 = GetDirCombos(combo1, [], (2, 0)).ToList();
-                var min2 = combos2.Min(c => c.Count);
-                if (min2 <= best2)
-                {
-                    best2 = min2;
-                    bestPresses = new([.. combos2.First(c => c.Count == min2)]);
-                }
-            }
-        }
-    }
-
-    return bestPresses;
+    return presses.LongLength;
 }
 
 long total = 0;
@@ -176,16 +102,16 @@ foreach (var line in File.ReadLines(file))
     var presses = GetPresses(line);
     var num = int.Parse(line[..3]);
     Console.WriteLine($"{line}: {presses}");
-    total += presses.Length * num;
+    total += presses * num;
 }
 
 Console.WriteLine(total);
 
-/*
-826A: <vA<AA>>^AvA<^A>AAAvA^A<vA<A>>^AA<A>vA^A<v<A>>^A<vA>A^A<A>A<vA<A>>^AA<A>vA^A
-341A: <v<A>>^AvA^A<vA<AA>>^AAvA<^A>AvA^A<vA<A>>^A<A>vA^A<vA>^A<v<A>>^AvA^A<A>A
-582A: <vA<AA>>^AvA<^A>AAvA^A<v<A>>^AvA^A<vA<A>>^AA<A>vA^A<vA<A>>^AvA^A<A>A
-983A: <v<A>>^AAAvA^A<vA<AA>>^AvAA<^A>A<vA<A>>^AAvA^A<A>A<vA<A>>^A<A>vA^A
-670A: <v<A>>^AAvA^A<vA<AA>>^AAvA<^A>AvA^A<vA>^A<v<A>>^AAA<A>vA^A<vA>^A<A>A
-237342
-*/
+enum DirPad : byte
+{
+    Enter = 0,
+    Left = 1,
+    Right = 2,
+    Up = 3,
+    Down = 4,
+}
